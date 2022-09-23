e0d237ce094b7e308fb86f950197581e8687a62e
sukhrobyangibaev <sukhrobyangibaev@gmail.com>
2022-09-23 11:16:08 +0500

    writing new id to players stat - adds player to fav list

--- main_menu/players_menu/players_menu.py
+++ main_menu/players_menu/players_menu.py
@@ -32,10 +32,28 @@ async def save_account_id(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     if response.status_code == 200 and "profile" in response.json():
         buttons = [["PLAYER'S MENU"], ["WRITE OTHER ID"], ["MAIN MENU"]]
 
-        context.user_data[ACCOUNT_ID] = account_id
         res_json = response.json()
-        context.user_data[PLAYER_NAME] = res_json["profile"]["personaname"]
-        text = "Player's name: " + res_json["profile"]["personaname"]
+        player_name = res_json["profile"]["personaname"]
+
+        context.user_data[ACCOUNT_ID] = account_id
+        context.user_data[PLAYER_NAME] = player_name
+
+        user_id = update.effective_user.id
+        fav_players = fav_players_col.find_one({"user_id": user_id})
+        player = {
+            "id": int(account_id),
+            "name": player_name
+        }
+        if not fav_players:
+            fav_players_col.insert_one({
+                "user_id": user_id,
+                "players": [player]
+            })
+        elif player not in fav_players["players"]:
+            fav_players["players"].append(player)
+            fav_players_col.update_one({"_id": fav_players["_id"]}, {"$set": fav_players})
+
+        text = "Player's name: " + player_name
     else:
         buttons = [["WRITE OTHER ID"], ["MAIN MENU"]]
         text = "wrong player's id"
