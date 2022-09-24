8f99994a0dbd90a996f79b34cfdb6d67174c6f5c
sukhrobyangibaev <sukhrobyangibaev@gmail.com>
2022-09-24 22:28:57 +0500

    fix pro player search

--- main_menu/pro_player/pro_player.py
+++ main_menu/pro_player/pro_player.py
@@ -1,7 +1,9 @@
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import ContextTypes
 
 from constants import PRO_PLAYER, TYPE_PRO_PLAYER, pro_players_col
 from helpers import helpers
+from main_menu import main_menu
 
 
 async def type_pro_player(update: Update, _) -> int:
@@ -9,16 +11,13 @@ async def type_pro_player(update: Update, _) -> int:
     return TYPE_PRO_PLAYER
 
 
-async def get_pro_player_name(update: Update, _) -> int:
+async def get_pro_player_name(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     player_name = update.message.text
-    buttons = [["WRITE OTHER PLAYER"], ["MAIN MENU"]]
-    keyboard = ReplyKeyboardMarkup(buttons)
-
     found_players = pro_players_col.find({"name": {"$regex": "^(?i)" + player_name}}).limit(10)
     text = helpers.pro_players_to_text(found_players)
     if len(text) == 0:
         text += "player not found"
 
-    await update.message.reply_html(text=text, reply_markup=keyboard)
+    await update.message.reply_html(text=text)
 
-    return PRO_PLAYER
+    return await main_menu(update, context)
