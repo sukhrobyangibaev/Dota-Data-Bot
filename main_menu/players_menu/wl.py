9f93e1f0ee2713310cf0f9e880c5aa1c655906c7
sukhrobyangibaev <sukhrobyangibaev@gmail.com>
2022-09-25 09:05:02 +0500

    fix wl

--- main_menu/players_menu/wl.py
+++ main_menu/players_menu/wl.py
@@ -1,15 +1,13 @@
 import requests
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update
 from telegram.ext import ContextTypes
 
-from constants import ACCOUNT_ID, WL
+from constants import ACCOUNT_ID
 from helpers import helpers
+from main_menu.players_menu import player_menu
 
 
 async def wl(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    button = [["BACK"]]
-    keyboard = ReplyKeyboardMarkup(button)
-
     account_id = context.user_data[ACCOUNT_ID]
     response = requests.get(f"https://api.opendota.com/api/players/{account_id}/wl")
     if response.status_code == 200:
@@ -18,5 +16,5 @@ async def wl(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         text += "\nWinrate: " + helpers.count_winrate(res_json["win"] + res_json["lose"], res_json["win"])
     else:
         text = "Error"
-    await update.message.reply_text(text=text, reply_markup=keyboard)
-    return WL
+    await update.message.reply_text(text=text)
+    return await player_menu(update, context)
