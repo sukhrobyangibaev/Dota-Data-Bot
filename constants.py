20d556073e0aa730736cbd8cb1feec2f5fe4f56d
sukhrobyangibaev <sukhrobyangibaev@gmail.com>
2023-03-22 11:34:51 +0500

    add logging file

--- constants.py
+++ constants.py
@@ -3,7 +3,9 @@ import logging
 import pymongo
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    filename='dota_data_bot.log',
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
